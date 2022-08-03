adec4ea6e57c634fc1e85596952f0fa6eca6b218
Wotanut <sambpilot@gmail.com>
2022-08-03 15:55:41 +0100

    file rename

---
+++ temperature_converter/main.java
@@ -0,0 +1,17 @@
+package temperature;
+import java.util.*;
+
+public class main {
+    public static void main(String[] args)
+    {
+        System.out.println("Enter the temperature in Fahrenheit: ");
+
+        Scanner input = new Scanner(System.in);
+        Integer farenheit = Integer.parseInt(input.nextLine());
+        input.close();
+
+        Integer celsius = 5 * (farenheit - 32) / 9;
+
+        System.out.println("The temperature in celsius is: " + celsius + " degrees");
+    }
+}
