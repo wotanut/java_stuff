acd17fe47530121f11afbc4f5f898fcc49a74582
Wotanut <sambpilot@gmail.com>
2022-08-04 09:45:03 +0100

    functions and converting back

--- temperature_converter/main.java
+++ temperature_converter/main.java
@@ -1,17 +1,52 @@
 package temperature_converter;
 import java.util.*;
 
+import javax.lang.model.util.ElementScanner14;
+
 public class main {
     public static void main(String[] args)
     {
-        System.out.println("Enter the temperature in Fahrenheit: ");
+        Integer result = null;
+
+        System.out.println("What do you want to convert too \n 1) Celcius \n 2) Farenheit ");
 
         Scanner input = new Scanner(System.in);
-        Integer farenheit = Integer.parseInt(input.nextLine());
+        String direction = input.nextLine();
+
+
+        if (direction.toLowerCase().equals("1") || direction.toLowerCase().equals("celcius"))
+        {
+            System.out.println("Please enter the temperature in farenheit ");
+            Integer i = Integer.parseInt(input.nextLine());
+
+            result = farenheit_to_celcius(i);
+        }
+        else if (direction.toLowerCase().equals("2") || direction.toLowerCase().equals("farenheit"))
+        {
+            System.out.println("Please enter the temperature in Celcius ");
+            Integer i = Integer.parseInt(input.nextLine());
+
+            result = clecius_to_farenheit(i);
+        }
+        else
+        {
+            System.out.println("Invalid input");
+            main(args=null);
+            System.exit(1);
+        }
+
+        System.out.println("The temperature in celsius is: " + result + " degrees");
         input.close();
+    }
 
-        Integer celsius = 5 * (farenheit - 32) / 9;
-
-        System.out.println("The temperature in celsius is: " + celsius + " degrees");
+    public static Integer farenheit_to_celcius(Integer farenheit)
+    {
+        Integer celcius = 5 * (farenheit - 32) / 9;
+        return celcius;
+    }
+    public static Integer clecius_to_farenheit(Integer celcius)
+    {
+        Integer farenheit = 9 * (celcius +32) /5;
+        return farenheit;
     }
 }
